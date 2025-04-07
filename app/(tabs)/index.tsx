e9cfd3c9494624f1d2a74825282f4864bff913d2
yacine <yacineyacine31212@gmail.com>
2025-04-07 09:46:57 +0100

    providers(nativeWind): ensure nativeWind work correctely

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,17 @@
-import { Image, StyleSheet, Platform , View, Text} from 'react-native';
-
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { View, Text, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../../global.css"
-import { verifyInstallation } from 'nativewind';
-
 
 export default function HomeScreen() {
 
-  verifyInstallation();
+
   return (
-    <View className="bg-white">
-    <Text>Hello, World!</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white" >
+      <Text className="text-red-500">hello world</Text>
+      <StatusBar style="auto"/>
+
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+
