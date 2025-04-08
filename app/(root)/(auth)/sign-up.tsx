e2b3eafb6c0fc307bd8877250c56f57eeda5cad6
yacine <yacineyacine31212@gmail.com>
2025-04-08 20:25:03 +0100

    components(InputFiled): finish reusable custom input filed

--- app/(root)/(auth)/sign-up.tsx
+++ app/(root)/(auth)/sign-up.tsx
@@ -1,20 +1,32 @@
 import { View, Text, ScrollView, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { images } from "@/constants";
-import InputField  from "@/components/InputField"
+import { images, icons } from "@/constants";
+import InputField from "@/components/InputField";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+  });
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-          <Text className="text-2xl text-black font-JakartaSemiBold sbsolute bottom-5 left-5">Create Your Acount</Text>
-
+          <Text className="text-2xl text-black font-JakartaSemiBold sbsolute bottom-5 left-5">
+            Create Your Acount
+          </Text>
         </View>
 
         <View className="p-5">
-          <InputField />
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
         </View>
       </View>
     </ScrollView>
--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,5 +1,48 @@
-const InputField = () => (
-<></>
-)
+import { InputFieldProps } from "@/types/type";
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  View,
+  Text,
+  Image,
+  TextInput,
+  Platform,
+  Keyboard,
+} from "react-native";
 
-export default InputField
+const InputField = ({
+  label,
+  labelStyle,
+  icon,
+  secureTextEntry = false,
+  containerStyle,
+  inputStyle,
+  iconStyle,
+  className,
+  ...props
+}: InputFieldProps) => (
+  <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View className="my2 w-full">
+        <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>
+          {label}
+        </Text>
+        <View
+          className={`flex flex-row justify-start items-center relative bg-neutral-100 
+            rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}
+        >
+          {icon && (
+            <Image source={icon} className={`h-6 w-6 ml-4 ${iconStyle}`} />
+          )}
+          <TextInput
+            className={`rounded-full p-4 font-JakartaSemiBold text-[15px] flex-1 ${inputStyle} text-left`}
+            secureTextEntry={secureTextEntry}
+            {...props}
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  </KeyboardAvoidingView>
+);
+
+export default InputField;
