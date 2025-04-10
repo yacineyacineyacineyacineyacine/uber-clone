b0ee3cd9bd5143a819a1733d44febf5ca896451c
yacine <yacineyacine31212@gmail.com>
2025-04-10 08:37:23 +0100

    config(authFlow): add rdirect to home page in the auth stack

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,9 +1,15 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function Layout() {
- 
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
+
   return (
-    <Stack screenOption={{headerShown: false}}>
+    <Stack screenOption={{ headerShown: false }}>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
       <Stack.Screen name="sign-in" options={{ headerShown: false }} />
       <Stack.Screen name="sign-up" options={{ headerShown: false }} />
