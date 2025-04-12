71c88b875108924d8425a9af987ca19acae8f0d4
yacine <yacineyacine31212@gmail.com>
2025-04-12 19:42:48 +0100

    fix(signed user): redirect user directely to home page when he is already loggedin

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SignOutButton } from "@/components/SignOutButton";
 
-const Home = () => {
+export default function Page() {
+  const { user } = useUser();
+
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
+    </View>
   );
-};
-
-export default Home;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import { useAuth } from "@clerk/clerk-expo";
 
 const Home = () => {
   const { isSignedIn } = useAuth();
+  console.log("isSignedIn: ", isSignedIn);
 
   if (isSignedIn) {
     return <Redirect href={"/(root)/(tabs)/home"} />;
