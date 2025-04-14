446cc0b61d8dc9ed3756cb91a33c7c60b2fad2a7
yacine <yacineyacine31212@gmail.com>
2025-04-14 21:36:09 +0100

    rideCard(header): finish to header for rides card

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -2,6 +2,7 @@ import { useUser } from "@clerk/clerk-expo";
 import { Text, View, FlatList } from "react-native";
 import { recentRides } from "@/constants";
 import { SafeAreaView } from "react-native-safe-area-context";
+import RideCard from "@/components/RideCard";
 
 export default function Home() {
   const { user } = useUser();
---
+++ components/RideCard.tsx
@@ -0,0 +1,48 @@
+import { icons } from "@/constants";
+import { Ride } from "@/types/type";
+import { Text, View, Image } from "react-native";
+
+const RideCard = ({
+  ride: {
+    destination_latitude,
+    destination_longitude,
+    origin_address,
+    destination_address,
+    created_at,
+    ride_time,
+    driver,
+    payment_status,
+  },
+}: {
+  ride: Ride;
+}) => (
+  <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
+    <View className="flex flex-row items-center justify-between p-3">
+      <View className="flex flex-row items-center justify-between">
+        <Image
+          source={{
+            uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+          }}
+          className="w-[80px] h-[90px] rounded-lg"
+        />
+        <View className="flex flex-col mx-5 gap-y-5 flex-1">
+          <View className="flex flex-row items-center gap-x-2">
+            <Image source={icons.to} className="w-5 h-5" />
+            <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+              {origin_address}
+            </Text>
+          </View>
+          <View className="flex flex-row items-center gap-x-2">
+            <Image source={icons.point} className="w-5 h-5" />
+            <Text className="text-md font-JakartaMedium">
+              {destination_address}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+    
+  </View>
+);
+
+export default RideCard;
