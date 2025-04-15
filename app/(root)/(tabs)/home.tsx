9b26ac5409b88c503ff740aa070a317777fc9149
yacine <yacineyacine31212@gmail.com>
2025-04-15 20:05:00 +0100

    feat(home): add ListEmptyConponent ListHeaderConponent isLoadingLogic signout functionality Welcome text

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,17 +1,102 @@
 import { useUser } from "@clerk/clerk-expo";
-import { Text, View, FlatList } from "react-native";
-import { recentRides } from "@/constants";
+import {
+  Text,
+  View,
+  FlatList,
+  Image,
+  ActivityIndicator,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+import { icons, images, recentRides } from "@/constants";
 import { SafeAreaView } from "react-native-safe-area-context";
 import RideCard from "@/components/RideCard";
+import { useClerk } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking";
+import GoogleTextInput from "@/components/GoogleTextInput";
 
 export default function Home() {
   const { user } = useUser();
+  const isLoading = false;
+  const { signOut } = useClerk();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      // Redirect to your desired page
+      Linking.openURL(Linking.createURL("/(auth)/sign-in"));
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  const handleDestinationPress = () => {};
 
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
         data={recentRides?.slice(0, 5)}
         renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!isLoading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  className="w-40 h-40 self-center"
+                  alt="No recent rides found"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm">No recent rides found</Text>
+              </>
+            ) : (
+              <ActivityIndicator
+                size="large"
+                color={Platform.OS === "ios" ? "#000" : "#0286ff"}
+              />
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-2xl capitalize font-JakartaBold">
+                Welcome{" "}
+                {user?.firstName ||
+                  user?.emailAddresses[0]?.emailAddress.split("@")[0]}{" "}
+                👋
+              </Text>
+              <TouchableOpacity
+                onPress={handleSignOut}
+                className="justify-center items-center w-10 h-10 rounded-full bg-white"
+              >
+                <Image source={icons.out} className="w-4 h-4" />
+              </TouchableOpacity>
+            </View>
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle="bg-white shadow-md shadow-neutral-300"
+              handlePress={handleDestinationPress}
+            />
+            <>
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Your Current Location
+              </Text>
+              <View className="flex flex-row items-center bg-trasparent h-[300px]">
+
+              </View>
+            </>
+          </>
+
+          // GoogleTextIput
+        )}
       />
     </SafeAreaView>
   );
---
+++ components/GoogleTextInput.tsx
@@ -0,0 +1,17 @@
+import { GoogleInputProps } from "@/types/type";
+import { View, Text } from "react-native";
+
+const GoogleTextInput = ({
+  icon,
+  initialLocation,
+  containerStyle,
+  textInputBackgroundColor,
+  handlePress,
+}: GoogleInputProps) => (
+  <View
+    className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}
+  >
+    <Text>Search</Text>
+  </View>
+);
+export default GoogleTextInput;
