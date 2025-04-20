76ef4f0e3949b206b368ec9ee0a106e46155ac9b
yacine <yacineyacine31212@gmail.com>
2025-04-20 18:46:03 +0100

    feat(map): style the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,13 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
-const Map = () => (
-  <MapView
-    provider={PROVIDER_DEFAULT}
-    style={{ width: "100%", height: "100%" }}
-  >
-    {/* <Text className="">Map</Text> */}
-  </MapView>
-);
+const Map = () => {
+  const region = {};
+
+  return (
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      style={{ width: "100%", height: "100%" }}
+      tintColor="black"
+      mapType={Platform.OS === "ios" ? "mutedStandard" : "standard"}
+      // initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    />
+  );
+};
 
 export default Map;
